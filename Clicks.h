f851c358955eadffbc3c60320719671e8569732b
MeshOK5 <144654697+MeshOK5@users.noreply.github.com>
2024-07-10 16:37:58 +0300

    Add files via upload

---
+++ Clicks.h
@@ -0,0 +1,21 @@
+#include <windows.h>
+#pragma once
+
+class Clicks
+{
+public:
+    bool click = false;
+    char type_click = 'L';
+
+    void Left_Click()
+    {
+        mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+        mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+    }
+
+    void Right_Click()
+    {
+        mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+        mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+    }
+};
